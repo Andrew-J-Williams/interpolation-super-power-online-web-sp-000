ee7d9080e353f446078b66fa355d027c349fe453
Andrew Williams <1williamsaj@gmail.com>
2019-08-18 14:00:27 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,6 +2,6 @@
 
 colors = ['red','orange','yellow','green','blue','indigo','violet'] #Our color array
 
-def display_rainbow(colors)
+def display_rainbow(colors) #We use string interpolation to take each value and put it in the string below
   puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
 end
